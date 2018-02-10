eafc85e3dd6b54082f92349f8539f5c5ba4422c3
matikitli <m.kitlinos@gmail.com>
2018-02-10 19:15:16 +0000

    Initial spring web commit

---
+++ src/main/java/com/CruiseExcursionBookingApp.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiseExcursionBookingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruiseExcursionBookingApp.class, args);
+    }
+
+}
